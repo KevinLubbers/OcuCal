6880d1406fe4196c38bf6ef1bb534854d44ed6ed
KevinLubbers <kelubbers@gmail.com>
2026-05-03 22:41:31 -0400

    reverted back to mousedown and mousenter Alpine actions instead of pointer, did not work well on mobile

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -142,10 +142,7 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link href="{{ route('calendar') }}" :active="request()->routeIs('calendar')">
-                {{ __('View Calendar') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link href="{{ route('adding') }}" :active="request()->routeIs('adding')">
-                {{ __('Add to Calendar') }}
+                {{ __('Calendar') }}
             </x-responsive-nav-link>
         </div>
 
